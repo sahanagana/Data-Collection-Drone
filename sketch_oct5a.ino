a7dce3db397498af392894a0433669a209870f01
sahanagana <70218712+sahanagana@users.noreply.github.com>
2021-10-05 19:20:47 -0500

    Add files via upload

---
+++ sketch_oct5a.ino
@@ -0,0 +1,19 @@
+void setup() {
+  // setting up input sensors
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+
+
+  //console for data testing
+  Console.begin()
+  //create bridge
+  Bridge.begin()
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
