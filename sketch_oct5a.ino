ea4047c75897f37c73023d2fc2c0666732075e0f
sahanagana <70218712+sahanagana@users.noreply.github.com>
2021-11-21 11:49:39 -0600

    Update sketch_oct5a.ino

--- sketch_oct5a.ino
+++ sketch_oct5a.ino
@@ -12,11 +12,23 @@
 ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL)
 
 void setup() {
-  // setting up input sensors
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
+    Serial.begin(115200);
+    /*Wait for the chip to be initialized completely, and then exit*/
+    while(sensor.begin() != 0){
+        Serial.println("failed to init chip, please check if the chip connection is fine");
+        delay(1000);
+    }
+    /**
+     * @brief Set measurement cycle
+     * @param cycle:in typedef enum{
+     *                  eClosed,      //Idle (Measurements are disabled in this mode)
+     *                  eCycle_1s,    //Constant power mode, IAQ measurement every second
+     *                  eCycle_10s,   //Pulse heating mode IAQ measurement every 10 seconds
+     *                  eCycle_60s,   //Low power pulse heating mode IAQ measurement every 60 seconds
+     *                  eCycle_250ms  //Constant power mode, sensor measurement every 250ms
+     *                  }eCycle_t;
+     */
+    sensor.setMeasCycle(sensor.eCycle_250ms);
 
   //connect to wifi
   thing.add_wifi(SSID, SSID_PASSWORD);
@@ -30,5 +42,26 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+void loop() {
+  delay(1000);
+    if(sensor.checkDataReady() == true){
+        Serial.print("CO2: ");
+        Serial.print(sensor.getCO2PPM());
+        Serial.print("ppm, TVOC: ");
+        Serial.print(sensor.getTVOCPPB());
+        Serial.println("ppb");
+        
+    } else {
+        Serial.println("Data is not ready!");
+    }
+    /*!
+     * @brief Set baseline
+     * @param get from getBaseline.ino
+     */
+    sensor.writeBaseLine(0x847B);
+    //delay cannot be less than measurement cycle
+    //delay(1000);
+}
 
 }
+
