e061ccde3e42c6acef9c575be384e589092588fa
sahanagana <70218712+sahanagana@users.noreply.github.com>
2021-11-30 22:05:59 -0600

    Create draft.ino

---
+++ draft.ino
@@ -0,0 +1,68 @@
+/*
+ * EEPROM Write
+ *
+ * Stores values read from analog input 0 into the EEPROM.
+ * These values will stay in the EEPROM when the board is
+ * turned off and may be retrieved later by another sketch.
+ */
+
+#include <EEPROM.h>
+//tvoc config
+//#include <Console.h>
+#include <CCS811.h>
+
+CCS811 sensor(&Wire /*IIC_ADDRESS=0x5A*/);
+
+/** the current address in the EEPROM (i.e. which byte we're going to write to next) **/
+int addr = 0;
+
+void setup() {
+    Serial.begin(115200);
+    /*Wait for the chip to be initialized completely, and then exit*/
+    while(sensor.begin() != 0){
+        Serial.println("failed to init chip, please check if the chip connection is fine");
+        delay(1000);
+    }
+    Serial.println("Enter a key to start");
+    /**
+     * @brief Set measurement cycle
+     * @param cycle:in typedef enum{
+     *                  eClosed,      //Idle (Measurements are disabled in this mode)
+     *                  eCycle_1s,    //Constant power mode, IAQ measurement every second
+     *                  eCycle_10s,   //Pulse heating mode IAQ measurement every 10 seconds
+     *                  eCycle_60s,   //Low power pulse heating mode IAQ measurement every 60 seconds
+     *                  eCycle_250ms  //Constant power mode, sensor measurement every 250ms
+     *                  }eCycle_t;
+     */
+    //sensor.setMeasCycle(sensor.eCycle_1s);
+
+  //console for data testing
+
+
+}
+
+
+void loop() {
+    if(Serial.available()){
+        String input = String(Serial.read());
+        Serial.print("You typed: " );
+        Serial.println(input);
+    }
+  delay(1000);
+  /*
+    if(sensor.checkDataReady() == true){
+        String val = "CO2: " + String(sensor.getCO2PPM()) + "ppm, TVOC: " + String(sensor.getTVOCPPB()) + "ppb ";
+        Serial.print(val);
+        EEPROM.put(addr, val);
+        addr += sizeof(val);
+    /*!
+     * @brief Set baseline
+     * @param get from getBaseline.ino
+     */
+    //sensor.writeBaseLine(0x847B);
+    //delay cannot be less than measurement cycle
+    //delay(1000);
+    //}
+
+  
+}
