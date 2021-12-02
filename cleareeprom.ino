6ef7c4cdfdc94b14ff7205c86d032dc381ec5be9
sahanagana <70218712+sahanagana@users.noreply.github.com>
2021-12-02 07:31:59 -0600

    Update cleareeprom.ino

--- cleareeprom.ino
+++ cleareeprom.ino
@@ -1,9 +1,8 @@
 /*
- * EEPROM Write
+ * EEPROM Clear
  *
- * Stores values read from analog input 0 into the EEPROM.
- * These values will stay in the EEPROM when the board is
- * turned off and may be retrieved later by another sketch.
+ * Clear all values in eeprom.
+ * Use after retrieving data.
  */
 
 #include <EEPROM.h>
